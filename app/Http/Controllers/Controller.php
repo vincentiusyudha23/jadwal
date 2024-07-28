f6bb2be69ce26ccda8c1d4db79ebddf71aab5440
Vincentius Yudha R <92367937+118140170Vincentius@users.noreply.github.com>
2024-07-29 00:38:11 +0700

    .

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
