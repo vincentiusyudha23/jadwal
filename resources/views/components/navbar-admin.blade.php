5b02ea3a02f5b8b50ccafa9b7b10d363fb5d5be7
Vincentius Yudha R <92367937+118140170Vincentius@users.noreply.github.com>
2025-04-01 03:10:15 +0700

    menu item mobile

--- resources/views/components/navbar-admin.blade.php
+++ resources/views/components/navbar-admin.blade.php
@@ -4,9 +4,7 @@
     <span class="fw-bold fs-5">{{ getCurrentTimeOfDay($name) }}</span>
     <div class="btn-group d-none d-sm-block">
         <button type="button" class="btn btn-outline-secondary btn-sm dropdown-toggle" data-bs-toggle="dropdown"
-            aria-expanded="false">
-            <i class="las la-user fs-5"></i>
-            {{ $name }}
+            aria-expanded="false">        {{ $name }}
         </button>
         <ul class="dropdown-menu">
             <li>
@@ -37,33 +35,57 @@
     <div class="mobile-menu bg-primary">
         <ul class="w-100 d-flex flex-column gap-2 list-menu-mb justify-content-center align-items-center py-3">
             @if (Auth::user()->hasRole('admin'))
-                <li>
-                    <a href="{{ route('admin.dashboard') }}" class="item-list-menu-mb">Halaman Utama</a>
-                </li>
-                <li>
-                    <a href="{{ route('admin.karyawan') }}" class="item-list-menu-mb">Data Karyawan</a>
-                </li>
-                <li>
-                    <a href="{{ route('admin.karyawan.jadwal') }}" class="item-list-menu-mb">Jadwal Karyawan</a>
-                </li>
-                <li>
-                    <a href="{{ route('admin.profile') }}" class="item-list-menu-mb">Ubah Password</a>
-                </li>
-                <li>
-                    <a href="{{ route('admin.history') }}" class="item-list-menu-mb">Riwayat Jadwal</a>
-                </li>
+            <li>
+                <a href="{{ route('admin.dashboard') }}" class="item-list-menu-mb">
+                    Halaman Utama
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.karyawan') }}"  class="item-list-menu-mb">
+                    Data Karyawan
+                </a>
+            </li>
+            <li>
+                <a href="#" class="item-list-menu-mb">
+                    Data Absensi
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.karyawan.jadwal') }}" class="item-list-menu-mb">
+                    Jadwal Karyawan
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.history') }}"  class="item-list-menu-mb">
+                    Riwayat Jadwal
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.penggajian') }}"  class="item-list-menu-mb">
+                    Penggajian
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.gaji.riwayat') }}" class="item-list-menu-mb">
+                    Riwayat Gaji
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.profile') }}"  class="item-list-menu-mb">
+                    Kelola Akun
+                </a>
+            </li>
             @endif
 
             @if (Auth::user()->hasRole('karyawan'))
-                <li>
-                    <a href="{{ route('karyawan.dashboard') }}" class="item-list-menu-mb">Halaman Utama</a>
-                </li>
-                <li>
-                    <a href="{{ route('karyawan.profile') }}" class="item-list-menu-mb">Ubah Password</a>
-                </li>
-                <li>
-                    <a href="{{ route('karyawan.jadwal.riwayat') }}" class="item-list-menu-mb">Riwayat Jadwal</a>
-                </li>
+                <li><a href="{{ route('karyawan.dashboard') }}" class="item-list-menu-mb">Halaman Utama</a></li>
+                <li><a href="{{ route('karyawan.absen.view') }}" class="item-list-menu-mb">Absensi Karyawan</a></li>
+                <li><a href="#" class="item-list-menu-mb">Riwayat Absensi</a></li>
+                <li><a href="{{ route('karyawan.jadwal.riwayat') }}" class="item-list-menu-mb">Riwayat Jadwal</a></li>
+                <li><a href="#" class="item-list-menu-mb">Riwayat Gaji</a></li>
+                <li><a href="#" class="item-list-menu-mb">Riwayat Ijin</a></li>
+                <li><a href="#" class="item-list-menu-mb">Riwayat Ijin</a></li>
+                <li><a href="{{ route('karyawan.profile') }}" class="item-list-menu-mb">Kelola Akun</a></li>
             @endif
         </ul>
     </div>
