6fa50353bfa07213cc85b6402ab0340895a04ed0
Vincentius Yudha R <92367937+118140170Vincentius@users.noreply.github.com>
2025-04-08 15:03:02 +0700

    script execute

---
+++ public/execute_queue.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/bin/ea-php82 /home/vyrsite1/jadwal/artisan queue:work --tries=3
---
+++ public/execute_schedule.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/bin/ea-php82 /home/vyrsite1/jadwal/artisan schedule:run >> /dev/null 2>&1
