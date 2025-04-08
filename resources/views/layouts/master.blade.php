cd5c8de1b3ed974ca4a71e911077cdef84104fd7
Vincentius Yudha R <92367937+118140170Vincentius@users.noreply.github.com>
2025-04-08 15:22:39 +0700

    add logo in tab browser

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title') | {{ config('APP_NAME', 'PT. Wira Griya') }}</title>
-
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo-1.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
